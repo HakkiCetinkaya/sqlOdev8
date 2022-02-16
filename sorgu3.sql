6befece27c0d61c678c9f9de6cd50c0df2a6db15
HakkiCetinkaya <hakki.cetinkaya@windowslive.com>
2022-02-16 03:27:50 +0300

    first commit

---
+++ sorgu3.sql
@@ -0,0 +1,29 @@
+UPDATE employee
+SET name = 'Ali',
+    birthday = '1981-05-02',
+    email = 'ali@ali.com'
+WHERE id = 11;
+
+UPDATE employee
+SET name = 'Veli',
+    birthday = '1990-04-04',
+    email = 'veli@veli.com'
+WHERE id = 17;
+
+UPDATE employee
+SET name = 'Kazim',
+    birthday = '1999-10-20',
+    email = 'kazim@kazim.com'
+WHERE id = 28;
+
+UPDATE employee
+SET name = 'Kadir',
+    birthday = '2000-12-23',
+    email = 'kadir@kadir.com'
+WHERE id = 32;
+
+UPDATE employee
+SET name = 'Bekir',
+    birthday = '1991-08-12',
+    email = 'bekir@bekir.com'
+WHERE id = 45;
---
+++ sorgu4.sql
@@ -0,0 +1,14 @@
+DELETE FROM employee
+WHERE id = 11;
+
+DELETE FROM employee
+WHERE name = 'Kazim';
+
+DELETE FROM employee
+WHERE email = 'bekir@bekir.com';
+
+DELETE FROM employee
+WHERE id = 32;
+
+DELETE FROM employee
+WHERE birthday = '1990-04-04';
